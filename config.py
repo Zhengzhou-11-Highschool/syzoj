8f4482416ad336a01301eaea33e27512e3632df9
Chenyao2333 <841144792@qq.com>
2015-10-25 23:57:36 +0800

    support display of math formula by using KaTex

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = "mysql://dev:fordev@192.168.3.15/syzoj"
+SQLALCHEMY_DATABASE_URI = "mysql://dev:fordev@192.168.3.4/syzoj"
 UPLOAD_FOLDER = os.path.join(BASE_DIR, "syzoj", "static", "uploads")
 JUDGE_TOKEN = "77783949202395150352388871624955475980489287735056"
 
